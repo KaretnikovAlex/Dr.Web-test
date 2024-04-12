02bbd540aa0e8bcd7a4cc09eb1c2abe963588999
KaretnikovAlex <shura.karetnikov.89@gmail.com>
2024-04-12 20:19:17 +0300

    add styles for logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,18 +5,27 @@ module.exports = {
     extend: {
       screens: {
         'mobile-1': '320px',
+        // => @media (min-width: 320px) { ... }
+
         'mobile-2': '480px',
+        // => @media (min-width: 480px) { ... }
+
         'mobile-3': '768px',
+        // => @media (min-width: 768px) { ... }
 
         'tablet': '640px',
         // => @media (min-width: 640px) { ... }
-  
+
         'laptop': '1024px',
         // => @media (min-width: 1024px) { ... }
-  
+
         'desktop': '1280px',
         // => @media (min-width: 1280px) { ... }
+
+        'large-desktop': '1950px',
+        // => @media (min-width: 1950px) { ... }
       },
+
     },
     plugins: [],
   }
